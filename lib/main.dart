74b5f3795246d777c532ec833a04b04940e5c59a
Rapeephat_Thamdee <Rapeephat.thamdee@e-tech.ac.th>
2025-12-01 16:05:01 +0700

    Add Chang Page2

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
       ),
-      initialRoute: '/',
+      initialRoute: '/NartheeSong',
       routes: {
-        '/':(context) => const MyHomepage(),
-        '/NartheeSong' : (context) =>  Nartheesong()
+        '/': (context) => const MyHomepage(),
+        '/NartheeSong': (context) => Nartheesong(),
       },
     );
   }
@@ -63,86 +63,165 @@ class MyHomepage extends StatelessWidget {
                       ),
                     ),
                     Text("Rapeephat Thamdee"),
-                    Text("rapeephat.thamdee@e-teh.ac.th"),
+                    Text(
+                      "rapeephat.thamdee@e-teh.ac.th",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
               ),
             ),
-            Container(child: Column(children: [
-              Padding(padding: EdgeInsets.all(8),child:Row(
-                children: [Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF72DD58),
-                    borderRadius: BorderRadius.circular(8),
+            Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "ข้อมูลส่วนตัว",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Icon(Icons.phone,size: 40,color: Color(0xFF45A125)
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF72DD58),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.phone,
+                            size: 40,
+                            color: Color(0xFF45A125),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("เบอร์"),
+                            Text(
+                              "085-5555565",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                  SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text("เบอร์"),Text("085-5555565")],)
-                ],
-              ),
-              ),
-              Padding(padding: EdgeInsets.all(8),child:Row(
-                children: [Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFDC8CD6),
-                    borderRadius: BorderRadius.circular(8),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFDC8CD6),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.cake,
+                            size: 40,
+                            color: Color(0xFFEF55A7),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("วันเกิด"),
+                            Text(
+                              "15 มกราคม 2549",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Icon(Icons.cake,size: 40,color: Color(0xFFEF55A7)
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFEAB96D),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.pin_drop,
+                            size: 40,
+                            color: Color(0xFFED8B1A),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("ที่อยู่"),
+                            Text(
+                              "ชลบุรี",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                  SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text("วันเกิด"),Text("15 มกราคม 2549")],)
-                ],
-              ),
-              ),
-              Padding(padding: EdgeInsets.all(8),child:Row(
-                children: [Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFEAB96D),
-                    borderRadius: BorderRadius.circular(8),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFA871C6),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Icons.school,
+                            size: 40,
+                            color: Color(0xFF8B1CCA),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("การศึกษา"),
+                            Text(
+                              "วิทยาลัยเทคโนโลยีภาคตะวันออก(อีเทค)",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Icon(Icons.pin_drop,size: 40,color: Color(0xFFED8B1A)
-                  ),
-                ),
-                  SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text("ที่อยู่"),Text("ชลบุรี")],)
-                ],
-              ),
-              ),
-              Padding(padding: EdgeInsets.all(8),child:Row(
-                children: [Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFA871C6),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Icon(Icons.pin_drop,size: 40,color: Color(0xFF8B1CCA)
-                  ),
-                ),
-                  SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text("ที่อยู่"),Text("ชลบุรี")],)
-                ],
-              ),
-              ),
 
-              ElevatedButton(onPressed: ()=>Navigator.pushNamed(context, '/NartheeSong'), child: Text("เปลี่ยนหน้า"),style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.orange
-              ),)
-            ])),
+                  ElevatedButton(
+                    onPressed: () =>
+                        Navigator.pushNamed(context, '/NartheeSong'),
+                    child: Text("เปลี่ยนหน้า"),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.orange,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -150,42 +229,147 @@ class MyHomepage extends StatelessWidget {
   }
 }
 
-class Nartheesong extends StatelessWidget{
+class Nartheesong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("NarTheeSong"),),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      ClipOval(
+                        child: Image.network(
+                          "https://i.pinimg.com/1200x/88/23/43/8823432b265ee5fbdeaaaf18c6075490.jpg",
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+
+                      SizedBox(width: 20),
+
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "5",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                "กำลังติดตาม",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 15),
+                          Column(
+                            children: [
+                              Text(
+                                "128.1 K",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                "ผู้ติดตาม",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 15),
+                          Column(
+                            children: [
+                              Text(
+                                "329.9 K",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                "ถูกใจและบันทึก",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Row(
+                    children: [
+                      Text(
+                        "Rapeephat Thamdee",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 6),
+                      Icon(Icons.verified, color: Colors.blue),
+                    ],
+                  ),
+                  Row(children: [Icon(Icons.tiktok),Text("Rapeephat", style: TextStyle(color: Colors.grey)),Icon(Icons.arrow_drop_down),],),
+                  
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Row(
+              children: [SizedBox(width: 20,),Container(
+
+                child: ElevatedButton(
+                onPressed: () => (),
+                child: Text("ตฺิดตาม"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.yellow,fixedSize: Size(300, 1)
+                ),
+              ),),
+                SizedBox(width: 30,),
+                Icon(Icons.share)
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  "https://i.pinimg.com/736x/e5/a0/0b/e5a00bb885be24479d816183057df974.jpg",
+                  width: 170,
+                  height: 170,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(width: 15),
+                Image.network(
+                  "https://i.pinimg.com/736x/e6/66/35/e666359bc1537c96cfeb344da2927077.jpg",
+                  width: 170,
+                  height: 170,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
-  
-  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
